2726cfc580d04d689909e2350981b49b8224022e
Tom Small <tsmall3@gmail.com>
2018-01-20 15:10:16 -0500

    elisp: implement hello-world solution

--- elisp/hello-world/hello-world.el
+++ elisp/hello-world/hello-world.el
@@ -3,7 +3,11 @@
 ;;; Commentary:
 
 ;;; Code:
-
+(defun hello (&optional name)
+  "Your basic hello world function."
+  (if name
+      (concat "Hello, " name "!")
+      "Hello, World!"))
 
 (provide 'hello-world)
 ;;; hello-world.el ends here
