888b74b4a3d3abbdb38bfb8256138d619f6596f8
Tom Small <tsmall3@gmail.com>
2018-01-21 13:07:23 -0500

    elisp: use `->>` to make solution easier to read

--- elisp/difference-of-squares/difference-of-squares.el
+++ elisp/difference-of-squares/difference-of-squares.el
@@ -3,17 +3,24 @@
 ;;; Commentary:
 
 ;;; Code:
+(load "~/.emacs.d/el-get/dash/dash.el")
+(require 'dash)
+
 (defun square (n)
   "Returns the square of number N."
   (expt n 2))
 
 (defun square-of-sums (limit)
   "Returns the square of the sums from 1 to LIMIT."
-  (square (apply '+ (number-sequence 1 limit))))
+  (->> (number-sequence 1 limit)
+       (apply '+)
+       (square)))
 
 (defun sum-of-squares (limit)
   "Returns the sum of the squares of numbers from 1 to LIMIT."
-  (apply '+ (mapcar 'square (number-sequence 1 limit))))
+  (->> (number-sequence 1 limit)
+       (mapcar 'square)
+       (apply '+)))
 
 (defun difference (limit)
   "Returns the difference between square-of-sums and
