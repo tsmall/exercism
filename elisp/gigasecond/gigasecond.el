4f5d25b717b71a87df5daaac91bbb28e6ac08008
Tom Small <tsmall3@gmail.com>
2018-01-23 21:00:12 -0500

    elisp: implement gigasecond solution

--- elisp/gigasecond/gigasecond.el
+++ elisp/gigasecond/gigasecond.el
@@ -9,11 +9,18 @@
 
 ;;; Code:
 
+(defconst one-gigasecond (expt 10 9)
+  "One gigasecond in seconds.")
 
+(defconst utc-timezone 0
+  "Representation of the UTC time zone.")
 
-
-
-
+(defun from (second minute hour day month year)
+  "Returns the time one gigasecond from the time provided."
+  (let* ((given-time (encode-time second minute hour day month year utc-timezone))
+         (later-time (time-add given-time one-gigasecond))
+         (decoded-time (decode-time later-time utc-timezone)))
+    (butlast decoded-time 3)))
 
 (provide 'gigasecond)
 ;;; gigasecond.el ends here
