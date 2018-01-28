bc9e5a0ac4dfbd1e538ae917c9c0805d18e153e1
Tom Small <tsmall3@gmail.com>
2018-01-27 22:23:32 -0500

    elisp: implement pangram solution

--- elisp/pangram/pangram.el
+++ elisp/pangram/pangram.el
@@ -4,6 +4,22 @@
 
 ;;; Code:
 
+(load "~/.emacs.d/el-get/dash/dash.el")
+(require 'dash)
+(require 'seq)
+
+(defun char-is-letter (c)
+  "Returns `t' if C is a letter (a-z, A-Z), `nil' otherwise."
+  (or (<= ?a c ?z)
+      (<= ?A c ?Z)))
+
+(defun is-pangram (sentence)
+  "Returns `t' if SENTENCE is a pangram, `nil' otherwise."
+  (->> (downcase sentence)
+       (seq-filter 'char-is-letter)
+       (delete-dups)
+       (length)
+       (= 26)))
 
 (provide 'pangram)
 ;;; pangram.el ends here
