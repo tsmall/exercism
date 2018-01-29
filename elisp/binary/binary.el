b867be5534c067435beb121cba51e1697f34b06a
Tom Small <tsmall3@gmail.com>
2018-01-29 00:10:43 -0500

    elisp: implement binary solution

--- elisp/binary/binary.el
+++ elisp/binary/binary.el
@@ -4,8 +4,24 @@
 
 ;;; Code:
 
+(require 'cl)
 
+(defun string-to-numbers (string)
+  "Convert every character in STRING to a number."
+  (mapcar 'string-to-number
+          (split-string string "" t)))
 
+(defun base-2-to-base-10 (numbers)
+  "Convert base-2 values in NUMBERS to their base-10 equavalents."
+  (loop for digit in numbers
+        for place from (1- (length numbers)) downto 0
+        collect (* digit (expt 2 place))))
+
+(defun to-decimal (binary)
+  "Returns a decimal representation of the BINARY string."
+  (let* ((base-2-digits (string-to-numbers binary))
+         (base-10-digits (base-2-to-base-10 base-2-digits)))
+    (apply '+ base-10-digits)))
 
 (provide 'binary)
 ;;; binary.el ends here
