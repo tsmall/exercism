046e7053425ffddec7cadd8b228e02a82772b6d1
Tom Small <tsmall3@gmail.com>
2018-01-29 08:06:43 -0500

    elisp: implement leap solution

--- elisp/leap/leap.el
+++ elisp/leap/leap.el
@@ -4,6 +4,16 @@
 
 ;;; Code:
 
+(defun divisible-by-p (number divisor)
+  "Returns `t' if NUMBER is evenly divisible by DIVISOR."
+  (= 0 (mod number divisor)))
+
+(defun leap-year-p (year)
+  "Returns `t' if YEAR is a leap year, `nil' otherwise."
+  (and (divisible-by-p year 4)
+       (or (not (divisible-by-p year 100))
+           (and (divisible-by-p year 100)
+                (divisible-by-p year 400)))))
 
 (provide 'leap)
 ;;; leap.el ends here
