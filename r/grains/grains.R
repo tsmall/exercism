85dee64d1cbeda11b5002b86e38199f8ec77cdbb
Tom Small <tsmall3@gmail.com>
2018-09-09 11:29:57 -0400

    r: implement grains solution

--- r/grains/grains.R
+++ r/grains/grains.R
@@ -1,7 +1,15 @@
 square <- function(n) {
-
+  stopifnot(n >= 1, n <= 64)
+  if (n == 1) {
+    1
+  }
+  else {
+    square(n - 1) * 2
+  }
 }
 
 total <- function() {
-
+  squares <- 1:64
+  grains_per_square <- sapply(squares, square)
+  sum(grains_per_square)
 }
