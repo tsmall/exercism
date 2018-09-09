0b7bd0543cabe99a7bce9a79a6a0c4df4bd46c4d
Tom Small <tsmall3@gmail.com>
2018-09-09 11:49:57 -0400

    r: implement leap solution

--- r/leap/leap.R
+++ r/leap/leap.R
@@ -1,3 +1,13 @@
 leap <- function(year) {
+  if (year %divisible_by% 400) {
+    return(TRUE)
+  }
+  if (year %divisible_by% 4) {
+    return(! year %divisible_by% 100)
+  }
+  return(FALSE)
+}
 
+`%divisible_by%` <- function(dividend, divisor) {
+  dividend %% divisor == 0
 }
