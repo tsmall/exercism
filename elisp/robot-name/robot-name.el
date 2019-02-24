46ca46ee217c5d00bf3eb6b17d8ea1abb7c4b49b
Tom Small <tsmall3@gmail.com>
2019-02-23 20:55:13 -0500

    elisp: implement robot-name solution

--- elisp/robot-name/robot-name.el
+++ elisp/robot-name/robot-name.el
@@ -8,6 +8,38 @@
 
 ;;; Code:
 
+(require 'eieio)
+
+(defclass robot ()
+  ((name :initarg :name
+         :type string)))
+
+(defun build-robot ()
+  "Builds a brand new robot."
+  (robot :name (generate-robot-name)))
+
+(defun robot-name (robot)
+  "Returns the ROBOT's current name."
+  (oref robot name))
+
+(defun reset-robot (robot)
+  "Resets the ROBOT's name to a newly generated one."
+  (oset robot name (generate-robot-name)))
+
+(defun generate-robot-name ()
+  "Generate a new robot name."
+  (concat
+   (string (random-char))
+   (string (random-char))
+   (number-to-string (random 9))
+   (number-to-string (random 9))
+   (number-to-string (random 9))))
+
+(defun random-char ()
+  "Returns a pseudo-random uppercase character (A-Z)."
+  (let* ((n (random 25))                 ; Get a random number
+         (c (+ n 65)))                   ; Make it in range A-Z
+    c))
 
 (provide 'robot-name)
 ;;; robot-name.el ends here
