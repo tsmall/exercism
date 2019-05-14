23f538adba43a7fdb505f897c23edb4d73d95a49
Tom Small <tsmall3@gmail.com>
2019-05-13 22:39:33 -0400

    elisp: implement etl solution

--- elisp/etl/etl.el
+++ elisp/etl/etl.el
@@ -4,6 +4,17 @@
 
 ;;; Code:
 
+(defun etl (old-format)
+  "Converts Scrabble scores from `OLD-FORMAT' to the new format."
+  (let ((new-format (make-hash-table)))
+    (maphash
+     (lambda (k v)
+       (when (< k 0) (error "Scores cannot be negative"))
+       (dolist (char v)
+         (when (not (stringp char)) (error "Letters must be strings"))
+         (puthash (downcase char) k new-format)))
+     old-format)
+    new-format))
 
 (provide 'etl)
 ;;; etl.el ends here
