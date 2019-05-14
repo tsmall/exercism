6d56954b8c5b9166730d9235e52ed7d2cb599a4a
Tom Small <tsmall3@gmail.com>
2019-05-13 23:05:23 -0400

    crystal: implement solution to hamming exercise

--- crystal/hamming/src/hamming.cr
+++ crystal/hamming/src/hamming.cr
@@ -1,1 +1,16 @@
-# Please implement your solution to hamming in this file
+module Hamming
+  extend self
+
+  def compute(strand_a, strand_b)
+    if strand_a.size != strand_b.size
+      raise ArgumentError.new("Strands must be the same length")
+    end
+
+    difference = 0
+    strand_a.each_char.zip(strand_b.each_char).each do |a, b|
+      difference += 1 if a != b
+    end
+    difference
+  end
+
+end
