4ab28470e501151aa7518849732db8fd4d17f24a
Tom Small <tsmall3@gmail.com>
2019-05-20 00:20:10 -0400

    dart: implement solution to resister-color exercise

--- dart/resistor-color/lib/resistor_color.dart
+++ dart/resistor-color/lib/resistor_color.dart
@@ -1,3 +1,10 @@
 class ResistorColor {
-  // Put your code here
+  List<String> colors = [
+    'black', 'brown', 'red',    'orange', 'yellow',
+    'green', 'blue',  'violet', 'grey',   'white',
+  ];
+
+  int colorCode(String colorName) {
+    return colors.indexOf(colorName);
+  }
 }
