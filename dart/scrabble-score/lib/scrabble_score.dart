a545d6e08d25e0c3d0cbda3a7b49fcf953677ea4
Tom Small <tsmall3@gmail.com>
2019-05-20 07:13:33 -0400

    dart: implement solution to scrabble-score exercise

--- dart/scrabble-score/lib/scrabble_score.dart
+++ dart/scrabble-score/lib/scrabble_score.dart
@@ -1,1 +1,19 @@
-// Put your code here
+const _scoreFor = {
+  'a' : 1, 'e' : 1, 'i' : 1, 'o' : 1, 'u' : 1,
+  'l' : 1, 'n' : 1, 'r' : 1, 's' : 1, 't' : 1,
+  'd' : 2, 'g' : 2,
+  'b' : 3, 'c' : 3, 'm' : 3, 'p' : 3,
+  'f' : 4, 'h' : 4, 'v' : 4, 'w' : 4, 'y' : 4,
+  'k' : 5,
+  'j' : 8, 'x' : 8,
+  'q' : 10, 'z' : 10,
+};
+
+int score(String word) {
+  var score = 0;
+  List<String> chars = word.split('');
+  chars.forEach((ch) {
+    score += _scoreFor[ch.toLowerCase()];
+  });
+  return score;
+}
--- dart/scrabble-score/test/scrabble_score_test.dart
+++ dart/scrabble-score/test/scrabble_score_test.dart
@@ -12,47 +12,47 @@ void main() {
     group('should return the appropriate score for', () {
       test('lowercase letters', () {
         expect(score('a'), equals(1));
-      }, skip: true);
+      }, skip: false);
 
       test('uppercase letters', () {
         expect(score('A'), equals(1));
-      }, skip: true);
+      }, skip: false);
 
       test('valuable letters', () {
         expect(score('f'), equals(4));
-      }, skip: true);
+      }, skip: false);
 
       test('english-like word', () {
         expect(score('pinata'), equals(8));
-      }, skip: true);
+      }, skip: false);
 
       test('long, mixed-case word', () {
         expect(score('OxyphenButazone'), equals(41));
-      }, skip: true);
+      }, skip: false);
 
       test('medium, valuable word', () {
         expect(score('quirky'), equals(22));
-      }, skip: true);
+      }, skip: false);
 
       test('medium word', () {
         expect(score('street'), equals(6));
-      }, skip: true);
+      }, skip: false);
 
       test('short, valuable word', () {
         expect(score('zoo'), equals(12));
-      }, skip: true);
+      }, skip: false);
 
       test('short word', () {
         expect(score('at'), equals(2));
-      }, skip: true);
+      }, skip: false);
 
       test('a word containing one of every letter of the alphabet', () {
         expect(score('abcdefghijklmnopqrstuvwxyz'), equals(87));
-      }, skip: true);
+      }, skip: false);
 
       test('a word containing one of every letter of the alphabet in reverse', () {
         expect(score('zyxwvutsrqponmlkjihgfedcba'), equals(87));
-      }, skip: true);
+      }, skip: false);
     });
   });
 }
