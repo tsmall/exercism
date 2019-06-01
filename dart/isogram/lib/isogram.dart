cec4bf6bd8d6790bd1881d3f993edb25cee54caa
Tom Small <tsmall3@gmail.com>
2019-06-01 12:46:33 -0400

    dart: implement solution to isogram exercise

--- dart/isogram/lib/isogram.dart
+++ dart/isogram/lib/isogram.dart
@@ -1,3 +1,25 @@
+final _aRune = 'a'.runes.first;
+
 class Isogram {
-  // Put your code here
+
+  bool isIsogram(String word) {
+    final haveSeenLetter = new List<bool>.filled(26, false);
+
+    int index;
+    for (int rune in word.toLowerCase().runes) {
+      index = rune - _aRune;
+      if (index < 0 || index > 25) {
+        continue;
+      }
+
+      if (haveSeenLetter[index]) {
+        return false;
+      }
+
+      haveSeenLetter[index] = true;
+    }
+
+    return true;
+  }
+
 }
