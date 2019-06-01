c46f1a148fee2ed5ee76bedafeafb2aa6c58c9e0
Tom Small <tsmall3@gmail.com>
2019-06-01 13:18:28 -0400

    dart: implement solution to isbn-verifier exercise

--- dart/isbn-verifier/lib/isbn_verifier.dart
+++ dart/isbn-verifier/lib/isbn_verifier.dart
@@ -1,3 +1,48 @@
+bool isValid(String isbn) {
+  final verifier = new IsbnVerifier(isbn);
+  return verifier.isValid;
+}
+
 class IsbnVerifier {
-  // Put your code here
+  String _code;
+
+  IsbnVerifier(String isbn) {
+    _code = _normalize(isbn);
+  }
+
+  String _normalize(String isbn) {
+    return isbn.replaceAll("-", "");
+  }
+
+  bool get isValid {
+    if (_code.length != 10) {
+      return false;
+    }
+
+    try {
+      return _computedValue % 11 == 0;
+    } on FormatException {
+      return false;
+    }
+  }
+
+  int get _computedValue =>
+    (_digit(1) * 10) +
+    (_digit(2) * 9) +
+    (_digit(3) * 8) +
+    (_digit(4) * 7) +
+    (_digit(5) * 6) +
+    (_digit(6) * 5) +
+    (_digit(7) * 4) +
+    (_digit(8) * 3) +
+    (_digit(9) * 2) +
+    (_digit(10) * 1);
+
+  int _digit(int index) {
+    String char = _code[index - 1];
+    if (index == 10 && char == "X") {
+      char = "10";
+    }
+    return int.parse(char);
+  }
 }
