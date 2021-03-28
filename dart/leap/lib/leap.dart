a2d2a77120c1bca9e12fc120067edd2421554ece
Tom Small <tsmall3@gmail.com>
2021-03-28 14:30:27 -0400

    dart: implement solution to leap exercise

--- dart/leap/lib/leap.dart
+++ dart/leap/lib/leap.dart
@@ -1,3 +1,15 @@
 class Leap {
-  // Put your code here
+  bool leapYear(int year) {
+    if (year.isDivisibleBy(100)) {
+      return year.isDivisibleBy(400);
+    } else {
+      return year.isDivisibleBy(4);
+    }
+  }
+}
+
+extension Division on int {
+  bool isDivisibleBy(int divisor) {
+    return this % divisor == 0;
+  }
 }
