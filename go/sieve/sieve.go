14b17e784be7bc5eab0a4635da12fc9ebc8b0476
Tom Small <tsmall3@gmail.com>
2021-10-13 21:30:10 -0400

    go: implement solution to sieve exercise

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,1 +1,33 @@
 package sieve
+
+func Sieve(limit int) []int {
+	if limit < 2 {
+		return []int{}
+	}
+
+	// We'll consider the default false value to mean the number IS prime,
+	// and mark the non-prime numbers as true.
+	markers := make([]bool, limit + 1)
+	markers[0] = true
+	markers[1] = true
+
+	for i := range markers {
+		if markers[i] { continue }
+
+		multiple := i + i
+		for multiple <= limit {
+			markers[multiple] = true
+			multiple += i
+		}
+	}
+
+	// Copy only the prime numbers into a new array.
+	var primes []int
+	for i, v := range markers {
+		if !v {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes
+}
