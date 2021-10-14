9e2b6faf0b6f579d0c2eb553901b662ba83f4777
Tom Small <tsmall3@gmail.com>
2021-10-13 22:27:09 -0400

    go: implement solution to hamming exercise

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,22 @@
 package hamming
 
+import "errors"
+
+// Distance calculates the Hamming Distance between the two DNA strands
+// represented by strings a and b. The two strings must be the same length;
+// if they are not, an error is returned.
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("strings must be the same length")
+	}
+
+	brs := []rune(b)
+	d := 0
+	for i, r := range a {
+		if r != brs[i] {
+			d++
+		}
+	}
+
+	return d, nil
 }
