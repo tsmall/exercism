d33ec38f8ddce11d27c4d1a24368cd9b931b2e2d
Tom Small <tsmall3@gmail.com>
2021-10-13 23:17:57 -0400

    go: implement solution to meetup exercise

--- go/meetup/meetup.go
+++ go/meetup/meetup.go
@@ -1,1 +1,23 @@
 package meetup
+
+import "time"
+
+type WeekSchedule int
+
+const (
+	First  WeekSchedule = 1
+	Second              = 8
+	Teenth              = 13
+	Third               = 15
+	Fourth              = 22
+	Last                = -6
+)
+
+func Day(sched WeekSchedule, weekday time.Weekday, month time.Month, year int) int {
+	if sched == Last {
+		month += 1
+	}
+
+	d := time.Date(year, month, int(sched), 0, 0, 0, 0, time.UTC)
+	return d.Day() + int(weekday-d.Weekday()+7)%7
+}
